addd1416bf272286e1265406508bf4f60bb6c5f8
Richard Neumann <r.neumann@homeinfo.de>
2023-07-25 22:47:52 +0200

    Black

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@
 from setuptools import setup
 
 setup(
-    name='urlshortener',
+    name="urlshortener",
     use_scm_version=True,
-    setup_requires=['setuptools_scm'],
-    author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info@homeinfo.de>',
-    maintainer='Richard Neumann',
-    maintainer_email='<r.neumann@homeinfo.de>',
-    py_modules=['urlshortener'],
-    requires=['basex', 'flask', 'peewee', 'peeweeplus', 'wsgilib'],
-    license='GPLv3',
-    description='URL shortener.'
+    setup_requires=["setuptools_scm"],
+    author="HOMEINFO - Digitale Informationssysteme GmbH",
+    author_email="<info@homeinfo.de>",
+    maintainer="Richard Neumann",
+    maintainer_email="<r.neumann@homeinfo.de>",
+    py_modules=["urlshortener"],
+    requires=["basex", "flask", "peewee", "peeweeplus", "wsgilib"],
+    license="GPLv3",
+    description="URL shortener.",
 )
--- urlshortener.py
+++ urlshortener.py
@@ -12,25 +12,25 @@ from peeweeplus import JSONModel, MySQLDatabaseProxy
 from wsgilib import JSON, JSONMessage
 
 
-__all__ = ['MANAGER', 'RESOLVER', 'ShortURL']
+__all__ = ["MANAGER", "RESOLVER", "ShortURL"]
 
 
-DATABASE = MySQLDatabaseProxy('urlshortener')
-MANAGER = Flask('manager')
+DATABASE = MySQLDatabaseProxy("urlshortener")
+MANAGER = Flask("manager")
 POOL = ascii_letters + digits
-EXCLUDE = '0oO1ilIL'
+EXCLUDE = "0oO1ilIL"
 
 for excluded in EXCLUDE:
-    POOL = POOL.replace(excluded, '')
+    POOL = POOL.replace(excluded, "")
 
-RESOLVER = Flask('resolver')
+RESOLVER = Flask("resolver")
 
 
 class ShortURL(JSONModel):
     """Table for shortened URLs."""
 
     class Meta:
-        table_name = 'short_url'
+        table_name = "short_url"
         database = DATABASE
         schema = database.database
 
@@ -50,11 +50,11 @@ class ShortURL(JSONModel):
     def to_json(self) -> dict[str, Union[int, str]]:
         """Return a JSON-ish dict."""
         json = super().to_json()
-        json['hash'] = self.hash
+        json["hash"] = self.hash
         return json
 
 
-@MANAGER.route('/', methods=['POST'], strict_slashes=False)
+@MANAGER.route("/", methods=["POST"], strict_slashes=False)
 def add_short_url() -> JSON:
     """Add a new short URL."""
 
@@ -67,27 +67,27 @@ def add_short_url() -> JSON:
     return JSON(short_url.hash)
 
 
-@MANAGER.route('/<hash_>', methods=['DELETE'])
+@MANAGER.route("/<hash_>", methods=["DELETE"])
 def delete_short_url(hash_: str) -> JSONMessage:
     """Delete a short URL."""
 
     try:
         short_url = ShortURL.by_hash(hash_)
     except ShortURL.DoesNotExist:
-        return JSONMessage('No such URL', status=404)
+        return JSONMessage("No such URL", status=404)
 
     short_url.delete_instance()
-    return JSONMessage('Short URL deleted.', status=200)
+    return JSONMessage("Short URL deleted.", status=200)
 
 
-@MANAGER.route('/', methods=['GET'], strict_slashes=False)
+@MANAGER.route("/", methods=["GET"], strict_slashes=False)
 def list_short_urls() -> JSON:
     """List short URLs."""
 
     return JSON([short_url.to_json() for short_url in ShortURL])
 
 
-@RESOLVER.route('/<hash_>', methods=['GET'])
+@RESOLVER.route("/<hash_>", methods=["GET"])
 def resolve_short_url(hash_: str) -> Response:
     """Resolve a short URL."""
 
